1f8496a64506820efc2703b9da27957369c9a7cf
Gopika-s-34 <sivagopika34@gmail.com>
2024-08-17 20:21:55 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,40 @@
+def encrypt_text(plaintext,n):
+    ans = ""
+    for i in range(len(plaintext)):
+        ch = plaintext[i]
+        if ch==" ":
+            ans+=" "
+        elif (ch.isupper()):
+            ans += chr((ord(ch) + n-65) % 26 + 65)
+        
+        else:
+            ans += chr((ord(ch) + n-97) % 26 + 97)
+    
+    return ans
+print("Caesar Cipher Encryption and Decryption")
+plaintext = input("enter a string:")
+n = 1
+print("Plain Text is : " + plaintext)
+print("Shift pattern is : " + str(n))
+print("Cipher Text is : " + encrypt_text(plaintext,n))
+
+def decrypt():
+    
+    encrypted_message = encrypt_text(plaintext,n)
+    letters="abcdefghijklmnopqrstuvwxyz"
+    k = int(input("Enter the key to decrypt: "))
+    decrypted_message = ""
+
+    for ch in encrypted_message:
+
+        if ch in letters:
+            position = letters.find(ch)
+            new_pos = (position - k) % 26
+            new_char = letters[new_pos]
+            decrypted_message += new_char
+        else:
+            decrypted_message += ch
+    print("Your decrypted message is:\n")
+    print(decrypted_message)
+
+decrypt()
